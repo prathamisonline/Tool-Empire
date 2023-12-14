5bc078cd1796395af7ef5f811d74d79375e6dca1
prathamisonline <prathamsrivastavas@gmail.com>
2023-12-14 22:49:22 +0530

    fixed lint error

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1>ToolEmpire</h1>
       <h1 >Innovative tools for empire builders.</h1>
-      <p>Welcome to a world of innovation and creativity! Discover a treasure trove of powerful tools designed to empower empire builders like you. Whether you're a designer, marketer, developer, or entrepreneur, our site offers an array of essential utilities at your fingertips.</p>
+      <p>Welcome to a world of innovation and creativity! Discover a treasure trove of powerful tools designed to empower empire builders like you. Whether you are a designer, marketer, developer, or entrepreneur, our site offers an array of essential utilities at your fingertips.</p>
     </main>
   )
 }
